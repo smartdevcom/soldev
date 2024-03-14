dcf3d60bde8d83408b98b0070d1285041ca0ad98
smartdevcom <smart.dev.cop@gmail.com>
2024-03-14 22:22:51 +0900

    lession 1

---
+++ generate-keypair.ts
@@ -0,0 +1,19 @@
+import { Keypair } from "@solana/web3.js";
+import "dotenv/config"
+
+import { getKeypairFromEnvironment } from "@solana-developers/helpers"
+
+const keypair1 = Keypair.generate();
+console.log(`The public key is: `, keypair1.publicKey.toBase58());
+console.log(`The secret key is: `, keypair1.secretKey);
+console.log(`✅ Finished!`);
+
+;
+
+const keypair2 = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(keypair2.publicKey.toBase58(), keypair2.secretKey)
+
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
